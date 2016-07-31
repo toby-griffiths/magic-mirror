bc6e0bddd178dc625a0923cf777f343c85bcf2bd
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-07-31 14:30:16 +0100

    Added ts:clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 'use strict';
 
-var gulp      = require('gulp'),
-    Config    = require('./gulp.config'),
-    tsc       = require('gulp-typescript'),
-    tsProject = tsc.createProject('tsconfig.json');
+var gulp   = require('gulp'),
+    Config = require('./gulp.config'),
+    tsc    = require('gulp-typescript'),
+    del    = require('del');
 
-var config = new Config();
+var tsProject = tsc.createProject('tsconfig.json'),
+    config    = new Config();
 
 gulp.task('setup', function () {
     gulp.src(['./gulp.config.ts'])
         .pipe(tsc(tsProject))
         .pipe(gulp.dest('.'));
+});
+
+gulp.task('ts:clean', function (cb) {
+    var typeScriptGenFiles = [
+        config.tsOutputPath + '/**/*.js',    // path to all JS files auto gen'd by editor
+        config.tsOutputPath + '/**/*.js.map', // path to all sourcemap files auto gen'd by editor
+        '!' + config.tsOutputPath + '/lib'
+    ];
+
+    // delete the files
+    del(typeScriptGenFiles, cb);
 });
