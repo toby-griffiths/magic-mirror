90e7cfe5358378e3e5a93d9aa78a8d78366eae02
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-07-31 14:09:23 +0100

    Loading gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var gulp      = require('gulp'),
+    Config    = require('./gulp.config'),
     tsc       = require('gulp-typescript'),
     tsProject = tsc.createProject('tsconfig.json');
+
+var config = new Config();
 
 gulp.task('setup', function () {
     gulp.src(['./gulp.config.ts'])
         .pipe(tsc(tsProject))
         .pipe(gulp.dest('.'));
 });
-
