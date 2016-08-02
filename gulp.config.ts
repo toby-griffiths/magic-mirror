ecf2360077fc3bef98df3429844944056677c303
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-02 13:17:45 +0100

    Generated JS now goes in build dir

--- gulp.config.ts
+++ gulp.config.ts
@@ -4,6 +4,8 @@
  * When editing the config, make sure you edit the gulp.config.ts file, rather than the generated .js file
  */
 class Config {
+    public build: string;
+    public buildJsFiles: string;
     public spec: string;
     public specAllTypeScript: string[];
     public specTsOutputPath: string;
@@ -16,14 +18,16 @@ class Config {
     public appTypeScriptReferences: string;
 
     constructor() {
+        this.build = "./build";
+        this.buildJsFiles = this.build + "/js";
         this.spec = "./spec";
         this.specAllTypeScript = [this.spec + "/**/*.ts"];
-        this.specTsOutputPath = this.spec;
-        this.specAllJavaScript = [this.spec + "/**/*.js"];
+        this.specTsOutputPath = this.buildJsFiles + "/spec";
+        this.specAllJavaScript = [this.specTsOutputPath + "/**/*.js"];
         this.source = "./src";
         this.srcAllTypeScript = [this.source + "/**/*.ts"];
-        this.srcTsOutputPath = this.source;
-        this.srcAllJavaScript = [this.source + "/**/*.js"];
+        this.srcTsOutputPath = this.buildJsFiles + "/src";
+        this.srcAllJavaScript = [this.srcTsOutputPath + "/**/*.js"];
         this.typings = "./typings";
         this.appTypeScriptReferences = this.typings + "/app.d.ts";
     }
