d32fa8598cc053243662a41fc235e7dfcf52280c
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-02 12:25:50 +0100

    Added initial QuestionSpec

---
+++ spec/ts/model/QuestionSpec.ts
@@ -0,0 +1,14 @@
+/* jslint node: true */
+/* global describe, it, expect */
+
+"use strict";
+
+import Question from "../../../src/ts/model/Question";
+
+describe("Question", function () {
+    it("has wording", function(){
+        let q = new Question("Question wording");
+
+
+    });
+});
