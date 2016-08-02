63126bfbb990ea5d445307d375c670827ba6f899
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-02 14:56:23 +0100

    Added output .js.map file patterns to config

--- gulp.config.ts
+++ gulp.config.ts
@@ -11,10 +11,12 @@ class Config {
     public specAllTypeScript: string[];
     public specTsOutputPath: string;
     public specAllJavaScript: string[];
+    public specAllJavaScriptMaps: string[];
     public source: string;
     public srcAllTypeScript: string[];
     public srcTsOutputPath: string;
     public srcAllJavaScript: string[];
+    public srcAllJavaScriptMaps: string[];
     public typings: string;
     public appTypeScriptReferences: string;
 
@@ -26,10 +28,12 @@ class Config {
         this.specAllTypeScript = [this.spec + "/**/*.ts"];
         this.specTsOutputPath = this.buildJsDir + "/spec";
         this.specAllJavaScript = [this.specTsOutputPath + "/**/*.js"];
+        this.specAllJavaScriptMaps = [this.specTsOutputPath + "/**/*.js.map"];
         this.source = "./src";
         this.srcAllTypeScript = [this.source + "/**/*.ts"];
         this.srcTsOutputPath = this.buildJsDir + "/src";
         this.srcAllJavaScript = [this.srcTsOutputPath + "/**/*.js"];
+        this.srcAllJavaScriptMaps = [this.srcTsOutputPath + "/**/*.js.map"];
         this.typings = "./typings";
         this.appTypeScriptReferences = this.typings + "/app.d.ts";
     }
