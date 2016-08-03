2923af3bd90ba095b2cc7f5cc1f35cfb06c787ba
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-03 12:13:55 +0100

    Stripped back main.ts ready for rework

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,4 @@
 ///<reference path="../typings/index.d.ts"/>
 
 window.onload = function () {
-    alert("Loaded");
 };
-
-new Vue({
-    el: "#app",
-    data: {
-        message: "Hello!"
-    }
-});
