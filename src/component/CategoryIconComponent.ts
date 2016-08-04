d3ac56fb5ee085d8a46734e0940ce908a097b0fb
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-04 17:41:06 +0100

    Added dynamicness to category icons

--- src/component/CategoryIconComponent.ts
+++ src/component/CategoryIconComponent.ts
@@ -1,10 +1,21 @@
 import Vue = require("../vue");
 
-const TEMPLATE = "Icons!";
+const TEMPLATE = "<div class='category-icon'>" +
+    "<img src='imgs/icon-love.png' v-if='shouldShowCategory(\"love\")'/>" +
+    "<img src='imgs/icon-prosperity.png' v-if='shouldShowCategory(\"prosperity\")'/>" +
+    "<img src='imgs/icon-body-mind.png' v-if='shouldShowCategory(\"body & mind\")'/>" +
+    "<img src='imgs/icon-mystery.png' v-if='shouldShowCategory(\"mystery\")'/>" +
+    "</div>";
 
 export var CategoryIconComponent = Vue.extend({
     template: TEMPLATE,
     props: ["category"],
+    methods: {
+        shouldShowCategory: function (category) {
+            console.log(category);
+            return (this.category && category === this.category.name);
+        }
+    }
 });
 
 Vue.component("category-icon", CategoryIconComponent);
