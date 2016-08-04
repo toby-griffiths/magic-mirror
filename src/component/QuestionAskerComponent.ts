ac588a694841d18332ee2bc05d758474206ee002
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-04 17:26:11 +0100

    Extended styling to Question page

--- src/component/QuestionAskerComponent.ts
+++ src/component/QuestionAskerComponent.ts
@@ -1,9 +1,12 @@
 import Vue = require("../vue");
 import {Answer} from "../model/Answer";
 
-const TEMPLATE = "<div class='question-asker'><h2>Question {{ questionNo }}</h2>" +
-    "<div class='question'>Question: {{ question.wording }}</div>" +
-    "<ul><li v-for='answer in question.answers' @click='answerSelected(answer)'>{{ answer.wording }}</li>" +
+const TEMPLATE = "<div class='question-asker'>" +
+    "<h2>{{ question.sequence }}. {{ question.wording }}</h2>" +
+    "<ul>" +
+    "<li v-for='answer in question.answers' @click='answerSelected(answer)'>" +
+    "<div class='answer'>{{ answer.wording }}</div>" +
+    "</li>" +
     "</ul></div>";
 
 export var CategorySelectorComponent = Vue.extend({
