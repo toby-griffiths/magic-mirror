bf48f487cdb32ed9efaa251d5942341a7a3da11d
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-06 21:29:51 +0100

    Setup basic websockets

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app     = express();
-var path    = require('path');
 var http    = require('http').Server(app);
+var io      = require('socket.io')(http);
 
-app.use(express.static(__dirname +'/../dist'));
+app.use(express.static(__dirname + '/../dist'));
+
+io.on('connection', function (socket) {
+
+    console.log('a user connected');
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
