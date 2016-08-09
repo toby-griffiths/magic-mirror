c5918fad6b190bc361e7f426de29ea4e33592a37
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-09 13:47:51 +0100

    Added the connecting page

--- web/ts/component/user/ConnectingScreen.ts
+++ web/ts/component/user/ConnectingScreen.ts
@@ -1,5 +1,15 @@
 import ComponentOption = vuejs.ComponentOption;
 
+const TEMPLATE = "<div class='screen-connecting'>" +
+    "<div class='content'>" +
+    "<div class='greeting'>Greetings <span class='name'>{{ userName }}</span></div>" +
+    "<div class='please-wait'>One moment, please...</div>" +
+    "</div>" +
+    "</div>";
+
 export const ConnectingScreen: ComponentOption = {
-    template: "ConnectingScreen"
+    template: TEMPLATE,
+    data: function () {
+        return {userName: this.$root.$get("userName")};
+    }
 };
