02c1ea511bf195165436ddd5d5cb3f2a304e9bd4
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-09 10:06:44 +0100

    Added main.ts & set questions & fortunes

--- web/ts/App.ts
+++ web/ts/App.ts
@@ -1,5 +1,8 @@
 ///<reference path="../../typings/index.d.ts"/>
 
+import {Category} from "./model/Category";
+import {Fortune} from "./model/Fortune";
+
 /**
  * Main client side App class
  *
@@ -14,12 +17,50 @@ export class App {
     private _socket: SocketIOClient.Socket;
 
     /**
+     * @type {CategoryList}
+     * @private
+     */
+    private _categories: CategoryList = {};
+
+    /**
+     * @type {FortuneMatrix}
+     */
+    private _fortunes;
+
+    /**
      * @constructor
      *
      * @param {string} _el
      */
     constructor(private _el: string) {
         this.socket = io();
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Data setting methods
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Adds a category to the available categories
+     *
+     * @param category
+     */
+    public addCategory(category: Category): void {
+        this.categories[category.name] = category;
+    }
+
+    /**
+     * Adds a fortune to the fortuens matrix
+     *
+     * @param {string} categoryName
+     * @param {Fortune} fortune
+     */
+    public addFortune(categoryName: string, fortune: Fortune) {
+        this.fortunes[categoryName] = this.fortunes[categoryName] || {};
+        this.fortunes[categoryName][fortune.answer1] = this.fortunes[categoryName][fortune.answer1] || {};
+        this.fortunes[categoryName][fortune.answer1][fortune.answer2] = this.fortunes[categoryName][fortune.answer1][fortune.answer2] || {};
+
+        this.fortunes[categoryName][fortune.answer1][fortune.answer2][fortune.answer3] = fortune;
     }
 
     // -----------------------------------------------------------------------------------------------------------------
@@ -46,4 +87,30 @@ export class App {
     set socket(value: SocketIOClient.Socket) {
         this._socket = value;
     }
+
+
+    get categories(): CategoryList {
+        return this._categories;
+    }
+
+    /**
+     * @return {FortuneMatrix}
+     */
+    get fortunes() {
+        return this._fortunes;
+    }
+}
+
+interface CategoryList {
+    [name: string]: Category;
+}
+
+interface FortuneMatrix {
+    [category: string]: {
+        [answer1: string]: {
+            [answer2: string]: {
+                [answer3: string]: Fortune
+            }
+        }
+    };
 }
