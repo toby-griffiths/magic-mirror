47b8005df851f7627ea17e4070907abc8062c16d
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-09 19:05:50 +0100

    Added style to categories page

--- web/ts/component/CategoriesScreen.ts
+++ web/ts/component/CategoriesScreen.ts
@@ -1,7 +1,15 @@
 import ComponentOption = vuejs.ComponentOption;
 
 const TEMPLATE = "<div class='screen-categories'>" +
-    "<div class='content'>CategoriesScreen</div>" +
+    "<div class='content'>" +
+    "<header>" +
+    "<h2>Select your category</h2>" +
+    "</header>" +
+    "<ul><li v-for='category in categories' @click='categorySelected(category)'>" +
+    "<div class='category'>{{category.name|capitalize}}</div>" +
+    "</li>" +
+    "</ul>" +
+    "</div>" +
     "</div>";
 
 export const CategoriesScreen: ComponentOption = {
