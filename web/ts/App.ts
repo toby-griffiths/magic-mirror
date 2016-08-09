3d83ac0b5ff3f170d522f060d83e698416042e33
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-08-09 08:47:39 +0100

    Initialise app with target element

---
+++ web/ts/App.ts
@@ -0,0 +1,16 @@
+/**
+ * Main client side App class
+ *
+ * Handles instantiating applicaiton
+ */
+export class App {
+
+    /**
+     * @constructor
+     *
+     * @param {string} _el
+     */
+    constructor(private _el: string) {
+    }
+
+}
